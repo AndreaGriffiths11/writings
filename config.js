4f99790a0ed00f786cbcfebdde29991796c8a91b
Andrea Griffiths <andreagriffiths11@github.com>
2019-11-11 07:19:08 -0800

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Andrea Liliana Griffiths',
   githubUrl: 'https://github.com/AndreaGriffiths11/writings',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: '[![Netlify Status](https://api.netlify.com/api/v1/badges/5dbcb322-d135-43dd-ae2e-f8984c8676e6/deploy-status)](https://app.netlify.com/sites/1111ag1111/deploys)',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'ALAColombia',
